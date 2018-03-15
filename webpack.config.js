2e3db0c58fa4be335b9a783b5c27f29b6222724d
Roi-Issam <33629850+Roi-Issam@users.noreply.github.com>
2018-03-15 11:46:03 +0100

    ajout sur le mac

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// webpack.config.js
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+// the project directory where all compiled assets will be stored
+    .setOutputPath('public/build/')
+
+    // the public path used by the web server to access the previous directory
+    .setPublicPath('/build')
+
+    // will create public/build/app.js and public/build/app.css
+    .addEntry('js/app', ['./node_modules/bootstrap/dist/js/bootstrap.js', './assets/js/app.js'])
+    .addStyleEntry('css/app', ['./node_modules/font-awesome/scss/font-awesome.scss', './assets/css/styles.css'])
+    .enableVersioning(true)
+
+    // allow sass/scss files to be processed
+    .enableSassLoader()
+
+    // allow legacy applications to use $/jQuery as a global variable
+    .autoProvidejQuery()
+
+    .enableSourceMaps(!Encore.isProduction())
+
+    // empty the outputPath dir before each build
+    .cleanupOutputBeforeBuild()
+
+    // show OS notifications when builds finish/fail
+    .enableBuildNotifications()
+
+// create hashed filenames (e.g. app.abc123.css)
+// .enableVersioning()
+;
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig();
